2c2f3b80eea41fc50aa0a81601a476afcd35a80d
Paul Huntzinger <phuntzinger@bluehost.com>
2020-10-08 11:09:04 -0600

    Added dry_run basic logic.

--- scripts/addons_importer.py
+++ scripts/addons_importer.py
@@ -1,30 +1,46 @@
 #!/bin/env python
+"""
+"""
 
 import json
 import os
 # from 'lib/helpers' import create_directory
 import helpers
+from helpers import dry_run
 
-with open('./config.json') as file:
-  data = json.load(file)
-  addons = data['addons']
-  shell = data['shell']
+def get_data():
+  with open('./config.json') as file:
+    data = json.load(file)
+    addons = data['addons']
+    shell = data['shell']
+  return data, addons, shell
 
-# --- Create Addons Directory ---
-helpers.create_directory('addons')
 
-# _create_directory('addons')
-# os.system('mkdir addons')
 
-# print(data['addons'])
-# print(data['shell'])
-# print(addons)
+def main():
+  if not dry_run('do this would happen now'):
+    print('do this')
 
-# --- Clone Addons ---
-for url in addons:
-  print('url: ' + url)
-  # os.system('cd addons; git clone ' + url + '; cd -')
-  # os.system(f'cd addons; git clone {url}; cd -')
-  os.system('cd addons; git clone {0}; cd -'.format(url))
+  data, addons, shell = get_data()
 
-print('Finished!')
+  # --- Create Addons Directory ---
+  helpers.create_directory('addons')
+
+  # _create_directory('addons')
+  # os.system('mkdir addons')
+
+  # print(data['addons'])
+  # print(data['shell'])
+  # print(addons)
+
+  # --- Clone Addons ---
+  for url in addons:
+    print('url: ' + url)
+    # os.system('cd addons; git clone ' + url + '; cd -')
+    # os.system(f'cd addons; git clone {url}; cd -')
+    os.system('cd addons; git clone {0}; cd -'.format(url))
+
+  print('Finished!')
+
+if __name__ == '__main__':
+  main()
--- scripts/helpers.py
+++ scripts/helpers.py
@@ -11,10 +11,18 @@
 # --- create_symlink("FILE_PATH") ---
 # This will create a symlink inside the $HOME directory for a given file path.
 
+import argparse
 import os
 import time
 
+FLAGS = argparse.ArgumentParser(__doc__)
+FLAGS.add_argument('--dry_run', action='store_true', help='Run without doing anything.')
+FLAGS = FLAGS.parse_args()
+
 def create_directory(dir):
+  if dry_run('create dir ' + dir):
+    return
+
   if not os.path.exists(dir):
     os.makedirs(dir)
 
@@ -22,7 +30,7 @@ def backup_file(file_path):
   """Backup file at a given path.
 
   args:
-    file_path: (str)
+    file_path: (str) Path to backup file.
   """
   if os.path.isfile(file_path):
     # get file name and containing directory
@@ -30,12 +38,19 @@ def backup_file(file_path):
     file_name = os.path.basename(file_path)
 
     # generate backup directory in file's directory (if one does not exist)
-    backup_path = "{0}/backups_rcs".format(dir_path)
+    backup_path = '{0}/backups_rcs'.format(dir_path)
     create_directory(backup_path)
 
     # move file to backup directory with epoch timestamp at end
     #   e.g. zshrc_1234567890
     timestamp = int(time.time())
-    dst_file_path = "{0}/{1}_{2}".format(backup_path, file_name, timestamp)
-    print("{0} backed up to: {1}".format(file_name, dst_file_path))
+    dst_file_path = '{0}/{1}_{2}'.format(backup_path, file_name, timestamp)
+    print('{0} backed up to: {1}'.format(file_name, dst_file_path))
     os.rename(file_path, dst_file_path)
+
+def dry_run(msg):
+  """Return True and print msg if dry_run flag is set, else return False."""
+  if FLAGS.dry_run:
+    print('dry_run: ' + msg)
+    return True
+  return False
