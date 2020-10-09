f8b92a9dd6e74108b2a7b24a69d95ac9258529f4
Paul Huntzinger <phuntzinger@bluehost.com>
2020-10-09 17:10:58 -0600

    master: Added prefix, sufix to backup_rcs.

--- lib/backup_rcs.py
+++ lib/backup_rcs.py
@@ -7,6 +7,8 @@ from helpers import backup_file, paths
 # rc_files = os.listdir('~/clither_custom/rcs')
 
 def main():
+  print('-' * 40)
+  print('Start backup_rcs...')
   dot_files = {x[1:] for x in os.listdir(paths.base_dir) if x.startswith('.')}
   rc_files = set(os.listdir(paths.rcs_path))
   dot_files_to_backup = dot_files.intersection(rc_files)
@@ -14,6 +16,8 @@ def main():
   for dot_file in dot_files_to_backup:
     backup_file_path = "{0}/.{1}".format(paths.base_dir, dot_file)
     backup_file(backup_file_path)
+
+  print('Finished backup_rcs!')
 
 if __name__ == "__main__":
   main()
