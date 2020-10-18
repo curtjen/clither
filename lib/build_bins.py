550486bc70f39314bc0a7f5c8e125cd8d32c3a5f
Paul Huntzinger <phuntzinger@bluehost.com>
2020-10-18 00:30:06 -0600

    master: Added some more comments.

--- lib/build_bins.py
+++ lib/build_bins.py
@@ -17,6 +17,9 @@ def _build_paths(config, addon_path):
       dst_full_path = os.path.join(paths.bin_path, file_name)
 
       print('will need to check if dst_full_path exists, if so do something dope')
+      # the dope thing would be is if it is a dup put in a review folder w/ + path name
+      # advantage is that it is really clear what exicutables there are and the path stays small
+      # but adding stuff is not as easy
       print('could do file: create_symlink({0}, {1})'.format(src_full_path, dst_full_path))
 
     # Option 2
@@ -36,7 +39,8 @@ def main():
   sys_bin_paths = sys_bin_paths.split(':')
   print('clear out everyting in clither_custom/bin')
   for sys_bin_path in sys_bin_paths:  # use enumerate and put on base name #_basename for new path, if option 2 pass last one in so can contineue, will also need to format to a 01 type thing
-    print('create_symlink(({0}, {1})'.format(sys_bin_path, paths.bin_path))
+    print('create_symlink(({0}, {1}), put these into an array so we an get the ###_ notation right'.format(sys_bin_path, paths.bin_path))
+    # Maybe the name should be ###_user+local+bin, etc
   print('make sure clither_custom/bin is in $PATH')
 
   # mk_clither_custom_dirs()
