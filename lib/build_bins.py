a5d2bf9743da634d498c9a1dd048e3b45d417b0a
Paul Huntzinger <phuntzinger@bluehost.com>
2020-10-18 13:56:14 -0600

    master: additional bin cleanup.

--- lib/build_bins.py
+++ lib/build_bins.py
@@ -8,7 +8,7 @@ from helpers import (paths, mk_clither_custom_dirs, append_to_file, clear_file,
 def get_new_path(path, prefix):
     #TODO(xnz): not system portable...
     new_path = path.replace('/', '+')
-    new_path = '{0}_{1}'.format(prefix, new_path)
+    new_path = '{0}+{1}'.format(prefix, new_path)
     new_path = os.path.join(paths.bin_path, new_path)
     return new_path
 
@@ -45,11 +45,23 @@ def _build_paths(config, addon_path):
 
         create_symlink(src, conflict_dst)
 
+    #for item in get_dir_list(paths.bin_path):
+    # if any([
+    #   item.startswith('addons+'),
+    #   re.match('^path_\d+', item),  # something like this?
+    #   os.isdir(path),
+    #   ]):
+    #   continue
+    #
+    # if item not in create_symlink:
+    #   rm item
+
 def _override(dir):
   pass
 
 def mk_paths_links():
   #print(os.environ)
+  print('Rm symlinks in paths.bin_path of the form ^path_\d+')
   print('Get content for $PATH, exclued clither_custom')  # not sure how to do this yet
   sys_bin_paths = '/Users/phuntzinger/.nvm/versions/node/v10.15.0/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:/usr/local/munki:/Users/phuntzinger/bin'
   sys_bin_paths = sys_bin_paths.split(':')
@@ -63,7 +75,7 @@ def mk_paths_links():
       # not sure this is right? but ok for now
       continue
 
-    new_path = get_new_path(sys_bin_path, index)
+    new_path = get_new_path(sys_bin_path, 'paths_' + str(index))
     create_symlink(sys_bin_path, new_path)
     # Maybe the name should be ###_user+local+bin, etc
 
