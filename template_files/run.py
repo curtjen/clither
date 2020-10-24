a591bf60d60c836c4000b52a40c2c2e35c5606c9
Paul Huntzinger <phuntzinger@bluehost.com>
2020-10-23 21:25:13 -0600

    master: Fixed dirs.

--- template_files/run.py
+++ template_files/run.py
@@ -10,14 +10,14 @@ def get_clither():
   """Get clither."""
   print('Getting clither...')
 
-  if os.path.exists(paths.clither_dir):
-    run_cmd(cd(paths.clither_dir), 'git pull')
+  if os.path.exists(paths.clither_path):
+    run_cmd(cd(paths.clither_path), 'git pull')
     return
 
-  run_cmd(cd(paths.parent_dir), 'git clone ' + paths.clither_base_repo)
+  run_cmd(cd(paths.base_dir), 'git clone ' + paths.clither_base_repo)
 
 def run_clither():
-  run_cmd(cd(paths.clither_dir), './clither.py --install')
+  run_cmd(cd(paths.clither_path), './clither.py --install')
 
 def main():
   get_clither()
